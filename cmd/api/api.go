f21c20914aeaa49f840f021123bbc480e75f60aa
Rami Mustaklem <ramimustaklem@gmail.com>
2024-08-31 06:39:12 +0300

    Initial commit

---
+++ cmd/api/api.go
@@ -0,0 +1,40 @@
+package api
+
+import (
+	"database/sql"
+	"github.com/sikozonpc/ecom/service/product"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sikozonpc/ecom/service/user"
+)
+
+type APIServer struct {
+	addr string
+	db   *sql.DB
+}
+
+func NewAPIServer(addr string, db *sql.DB) *APIServer {
+	return &APIServer{
+		addr: addr,
+		db:   db,
+	}
+}
+
+func (s *APIServer) Run() error {
+	router := mux.NewRouter()
+	subRouter := router.PathPrefix("/api/v1").Subrouter()
+
+	userStore := user.NewStore(s.db)
+	userHandler := user.NewHandler(userStore)
+	userHandler.RegisterRoutes(subRouter)
+
+	productStore := product.NewStore(s.db)
+	productHandler := product.NewHandler(productStore)
+	productHandler.RegisterRoutes(subRouter)
+
+	log.Println("Listening on", s.addr)
+
+	return http.ListenAndServe(s.addr, router)
+}
---
+++ service/product/store.go
@@ -0,0 +1,82 @@
+package product
+
+import (
+	"database/sql"
+	"github.com/sikozonpc/ecom/types"
+)
+
+type Store struct {
+	db *sql.DB
+}
+
+func NewStore(db *sql.DB) *Store {
+	return &Store{db: db}
+}
+
+func (s *Store) CreateProduct(product types.Product) error {
+	_, err := s.db.Exec("INSERT INTO products (name, description, image, price, quantity) VALUES (?, ?, ?, ?, ?)", product.Name, product.Description, product.Image, product.Price, product.Quantity)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (s *Store) GetProductByID(id int) (*types.Product, error) {
+	rows, err := s.db.Query("SELECT * FROM products WHERE id = ?", id)
+	if err != nil {
+		return nil, err
+	}
+
+	p := new(types.Product)
+	for rows.Next() {
+		p, err = scanRowsIntoProduct(rows)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	if p.ID == 0 {
+		return nil, nil
+	}
+
+	return p, nil
+}
+
+func (s *Store) GetProducts() ([]types.Product, error) {
+	rows, err := s.db.Query("SELECT * FROM products")
+	if err != nil {
+		return nil, err
+	}
+
+	products := make([]types.Product, 0)
+	for rows.Next() {
+		p, err := scanRowsIntoProduct(rows)
+		if err != nil {
+			return nil, err
+		}
+
+		products = append(products, *p)
+	}
+
+	return products, nil
+}
+
+func scanRowsIntoProduct(rows *sql.Rows) (*types.Product, error) {
+	product := new(types.Product)
+
+	err := rows.Scan(
+		&product.ID,
+		&product.Name,
+		&product.Description,
+		&product.Image,
+		&product.Price,
+		&product.Quantity,
+		&product.CreatedAt,
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return product, nil
+}
